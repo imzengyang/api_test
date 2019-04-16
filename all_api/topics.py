b0c7c4acc34588afa78c2676482c4d1eeda583bb
zack <zengyangzhao@outlook.com>
2019-04-16 08:59:48 +0800

    add post test api

--- all_api/topics.py
+++ all_api/topics.py
@@ -10,3 +10,25 @@ class Topics(object):
         params = {"limit":limit,"tab":tab}
         r = requests.get(self.url,params=params)
         return  r
+    
+    def post_create_topic(self,token,title,tab,content):
+        params = {
+            "accesstoken":token,
+            "title":title,
+            "tab":tab,
+            "content":content
+        }
+        r = requests.post(self.url,data=params)
+        return r
+    
+    def post_update_topic(self,token,topic_id,title,tab,content):
+        params = {
+            "accesstoken":token,
+            "topic_id":topic_id,
+            "title":title,
+            "tab":tab,
+            "content":content
+        }
+        r = requests.post(self.url,data=params)
+        return r
+
--- test_cases/test_topics.py
+++ test_cases/test_topics.py
@@ -2,8 +2,14 @@ import pytest
 from all_api.topics import Topics
 
 
+default_token = "3333a0fb-6dd8-439e-813b-2c3a5213a154"
+
 @pytest.mark.parametrize("limit,tab", [("1", "ask"), ("10", "share"), ("5", "job")])
 def test_index_page(limit,tab):
+    """
+    获取主题首页帖子api 测试
+    测试不同参数返回不同服务器数据
+    """
     url = "/topics"
     topics = Topics(url)
     # assert_limit=1
@@ -16,3 +22,12 @@ def test_index_page(limit,tab):
     all_data = r['data']
     for data in all_data:
         assert data['tab'] == tab
+    
+@pytest.mark.parametrize("token,title,tab,content", [(default_token, "helloworld1111","ask","xxxxxxxxxxxxxx"), (default_token, "helloworld1111","ask","xxxxxxxxxxxxxx")])
+def test_post_create_topic(token,title,tab,content):
+    url = '/topics'
+    topic = Topics(url)
+    res = topic.post_create_topic(token,title,tab,content)
+    assert res.status_code == 200
+    r = res.json()
+    assert r['success'] == True
